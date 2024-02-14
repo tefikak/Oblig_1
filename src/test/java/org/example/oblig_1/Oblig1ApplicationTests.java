cdc2b7d2df8fc0b956a4cfa816fd4b8d5684bcf0
Tefik Akyüz <143293501+tefikak@users.noreply.github.com>
2024-02-14 09:29:13 +0100

    Initial commit

---
+++ src/test/java/org/example/oblig_1/Oblig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oblig_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
